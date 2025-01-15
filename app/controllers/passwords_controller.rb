6c58f95f7eee3e68345dfaf408de45a211f03f1f
vitor <vitor.grosskopf@gmail.com>
2025-01-15 16:45:12 -0300

    Create controller: PasswordsController
    - Create actions: index, show, new and create
    - Adds strong params, and user authentication on before_action

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,36 @@
+class PasswordsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_password, except: [:index, :new, :create]
+  def index
+    @passwords = current_user.passwords
+  end
+
+  def show
+  end
+  def new
+    @password = Password.new
+  end
+
+  def create
+    # @password = Password.new(password_params)
+    # @password.user_passwords.new(user: current_user)
+    # if @password.save
+
+    @password = current_user.passwords.create(password_params)
+    if @password.persisted?
+      redirect_to @password
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:password).permit(:url, :username, :password)
+  end
+
+  def set_password
+    @password = current_user.passwords.find(params[:id])
+  end
+end
