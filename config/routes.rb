f5fe5d78d3065e7e34711d5e5b77bd01af4fdac5
vitor <vitor.grosskopf@gmail.com>
2025-01-15 16:39:50 -0300

    Update routes
     - Add routes and resources for passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :passwords
 
+  root "passwords#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
