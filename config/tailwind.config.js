86b72678cb2304d63aa033b7df3824bc9d789dc0
vitor <vitor.grosskopf@gmail.com>
2025-01-15 22:05:33 -0300

    Adds Tailwind-css plugins

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = {
     },
   },
   plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
