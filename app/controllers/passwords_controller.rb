f399d67f5981265cbd2819c90992b4c744a0ac99
vitor <vitor.grosskopf@gmail.com>
2025-01-20 14:50:44 -0300

    Fix typo

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,23 +1,25 @@
 class PasswordsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_password, except: [:index, :new, :create]
+
   def index
     @passwords = current_user.passwords
   end
 
   def show
   end
+
   def new
     @password = Password.new
   end
 
   def create
-    # @password = Password.new(password_params)
-    # @password.user_passwords.new(user: current_user)
-    # if @password.save
+    @password = Password.new(password_params)
+    @password.user_passwords.new(user: current_user)
+    if @password.save
 
-    @password = current_user.passwords.create(password_params)
-    if @password.persisted?
+    # @password = current_user.passwords.create(password_params)
+    # if @password.persisted?
       redirect_to @password
     else
       render :new, status: :unprocessable_entity
@@ -30,7 +32,7 @@ class PasswordsController < ApplicationController
       if @password.update(password_params)
         redirect_to @password
       else
-        render :edit, stastus: :umprocessable_entity
+        render :edit, status: :unprocessable_entity
       end
     end
 
