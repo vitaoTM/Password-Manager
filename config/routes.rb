1a7c03e60d051087e0bd35221f4685840d4e45df
vitor <vitor.grosskopf@gmail.com>
2025-01-20 14:51:37 -0300

    Nothing relevant
    - Just change lines out of place

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
   resources :passwords
 
-  root "passwords#index"
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "passwords#index"
 end
