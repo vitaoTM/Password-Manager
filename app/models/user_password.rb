f5166dbadf1ca998f43ad134cecc0a2b0884ec11
vitor <vitor.grosskopf@gmail.com>
2025-01-28 17:36:16 -0300

    UserPassword model: Add method
    - Add owner?, viewer?, and editor? methods to determine the role of the user for a specific password

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -4,4 +4,33 @@ class UserPassword < ApplicationRecord
   belongs_to :password
 
   validates :role, presence: true, inclusion: { in: ROLES }
+
+  attribute :role, default: :viewer
+
+  def editable?
+    owner? || editor?
+  end
+
+  def shareable?
+    owner?
+  end
+
+  def deletable?
+    owner?
+  end
+
+  private
+
+  def owner?
+    role == "owner"
+  end
+
+  def viewer?
+    role == 'viewer'
+  end
+
+  def editor?
+    role == 'editor'
+  end
+
 end
