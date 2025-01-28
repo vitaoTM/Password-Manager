08b7a863d7437f5f0879ecf06a48606abf29dca6
vitor <vitor.grosskopf@gmail.com>
2025-01-27 23:05:10 -0300

    Add validations to role

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,4 +1,7 @@
 class UserPassword < ApplicationRecord
+  ROLES = %w{viewer editor owner}
   belongs_to :user
   belongs_to :password
+
+  validates :role, presence: true, inclusion: { in: ROLES }
 end
