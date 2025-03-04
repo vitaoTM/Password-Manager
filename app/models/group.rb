5cf3a32dd07c869ef9fcff602c17c97aa5a3c106
vitor <vitor.grosskopf@gmail.com>
2025-03-04 19:43:45 -0300

    Adds validation on Group model
    - render group partial on index

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
-  has_many :users
+  has_many :users, dependent: :nullify
   has_many :user_passwords, through: :users, dependent: :destroy
   has_many :passwords, through: :user_passwords, dependent: :destroy
+
+  validates :name, presence: true
 end
