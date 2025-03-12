d8f86d4de5505b1901c2f8444bb6bdcecea82130
vitor <vitor.grosskopf@gmail.com>
2025-03-12 01:15:00 -0300

    Thinking User shoud have other associations
    - commented for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # has_and_belongs_to_many :groups
   belongs_to :group, optional: true
+
+  # has_many :groups, optional: true
   has_many :user_passwords, dependent: :destroy
   has_many :passwords, through: :user_passwords
 end
